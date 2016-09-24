6c32dd5631406e9286b14725f475e3c1383ba12c
Adrian Catalan <acatalan@hiberus.com>
2016-09-24 10:17:36 +0200

    Chapter5_Services

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,17 @@
 import {Component} from 'angular2/core';
+import {Component1Component} from "./component1.component";
+import {Component2Component} from "./component2.component";
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: `
-        <h1>Angular 2</h1>
-        <p>Hello World!</p>
+        <h1>First Component</h1>
+        <component-1></component-1>
+        <br>
+        <h1>Second Component</h1>
+        <component-2></component-2>
     `,
+    directives: [Component1Component, Component2Component]
 })
 export class AppComponent {
 
