717f516371d46c98669770d11ef3d94eee909ed6
Adrian Catalan <acatalan@hiberus.com>
2016-10-29 12:32:49 +0200

    Filter ShoppingCart

--- dev/shopping-list.component.ts/shopping-list.component.ts
+++ dev/shopping-list.component.ts/shopping-list.component.ts
@@ -3,6 +3,7 @@ import {ShoppingListNewItemComponent} from "./shopping-list-new-item.component";
 import {ListItem} from "../list-item";
 import {ShoppingListItemComponent} from "./shopping-list-item-component";
 import {ShoppingListService} from "./shopping-list-service";
+import {FilterPipe} from "../filter.pipe";
 
 @Component({
     selector: 'shopping-list',
@@ -13,9 +14,11 @@ import {ShoppingListService} from "./shopping-list-service";
         </section>
         <section>
             <h3>My List</h3>
+            Filter:
+            <input type="text" #filter (keyup)="0">
             <div class="list">
                 <ul>
-                <li *ngFor="#listItem of listItems" (click)="onSelect(listItem)">
+                <li *ngFor="#listItem of listItems | myFilter:filter.value" (click)="onSelect(listItem)">
                     {{listItem.name}} ({{listItem.amount}})
                 </li>
                 </ul>
@@ -26,7 +29,8 @@ import {ShoppingListService} from "./shopping-list-service";
         </section>
     `,
     directives: [ShoppingListNewItemComponent, ShoppingListItemComponent],
-    providers: [ShoppingListService]
+    providers: [ShoppingListService],
+    pipes: [FilterPipe]
 })
 export class ShoppingListComponent implements OnInit{
 
